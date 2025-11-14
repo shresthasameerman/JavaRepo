a4727bc6d337e815b2da9e5c88df3832e96be946
sameer man shrestha <shresthasameerman@gmail.com>
2025-11-14 09:02:24 +0545

    Java Lecture 2

--- src/StudentGradeManager.java
+++ src/StudentGradeManager.java
@@ -1,57 +1,46 @@
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+
 public class StudentGradeManager {
 
-    /**
-     * A map which maps from a key (student ID) to a value (module grade)
-     */
-    private Map<String, Float> moduleMarks = new HashMap<String, Float>();
+    private Map<Integer, Float> moduleMarks = new HashMap<Integer, Float>();
+    private Map<Integer, String> studentEmails = new HashMap<Integer, String>();
 
-    /**
-     * Adds a student ID and associated grade
-     * @param studentID
-     * @param grade
-     */
-    void addGrade(String studentID, float grade) {
-
+    void addGrade(int studentID, float grade) {
         moduleMarks.put(studentID, grade);
     }
-    /**
-     * Gets the grade associated with the given student
-     * @param studentID
-     * @return the student's grade
-     */
-    float getGrade(String studentID) {
 
+    void addStudent(int studentID, float grade, String email) {
+        moduleMarks.put(studentID, grade);
+        studentEmails.put(studentID, email);
+    }
+
+    Float getGrade(int studentID) {
         return moduleMarks.get(studentID);
     }
 
-    void printAllGrades() {
-
-        System.out.println("The student grades are as follows - ");
-
-        for(Entry<String, Float> entry : moduleMarks.entrySet()) {
-
-            String studentID = entry.getKey();    // get the key
-            float grade = entry.getValue();    // get the associated value
-
-            System.out.println(studentID + " : " + grade);
-        }
-
+    String getEmail(int studentID) {
+        return studentEmails.get(studentID);
     }
 
-    public static void main(String[] args){
-        StudentGradeManager students=new StudentGradeManager();
-        students.addGrade("1A",3.6f);
-        students.addGrade("2b",3.45f);
+    void printAllGrades() {
+        System.out.println("The student grades are as follows - ");
+        for (Entry<Integer, Float> entry : moduleMarks.entrySet()) {
+            Integer studentID = entry.getKey();
+            Float grade = entry.getValue();
+            System.out.println(studentID + " : " + grade);
+        }
+    }
 
-
+    public static void main(String[] args) {
+        StudentGradeManager students = new StudentGradeManager();
+        students.addStudent(1, 3.6f, "student1@example.com");
+        students.addStudent(2, 3.45f, "student2@example.com");
 
         students.printAllGrades();
 
-        System.out.println(students.getGrade("1A"));
-
+        // show the email of the 2nd student using roll number 2
+        System.out.println("Email of 2: " + students.getEmail(2));
     }
 }
